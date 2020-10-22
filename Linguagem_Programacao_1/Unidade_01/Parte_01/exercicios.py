d55cc98295b2535a70094767d930ed27a6f20a6b
juzapata <juliamzapata@gmail.com>
2020-10-22 15:24:14 -0300

    primeira unidade

---
+++ Linguagem_Programacao_1/Unidade_01/Parte_01/exercicios.py
@@ -0,0 +1,108 @@
+# print("Hello World") 
+
+# A = 10
+# print(A)
+
+
+# Exercicio 3
+
+# numberOne = int(input("Type a number"))
+# numberTwo = int(input("Type a second number"))
+
+# soma = numberOne + numberTwo
+
+# print("The sum numbers is", soma)
+
+
+# Exercício proposto 2
+# Escrever, em Python, um programa que calcula e apresenta o dobro, o triplo e o quadrado de um número. 
+
+# number = int(input("Type a number "))
+
+# double = number * 2
+# triple = number * 3
+# square = number * number
+
+# print(double 
+# print(triple)
+# print(square)
+
+# LISTA DE EXERCICIOS
+
+
+# UM ^^^^^^^^^^^^^^^^^^^^^^
+# Faça um programa que peça as 4 notas de atividades contínuas e mostre a média. Lembrando que, o
+# total de atividades contínuas consideradas para o cálculo da média será um total de sete). 5 + 10 + 6 + 7 = 28 
+
+# gradeOne = int(input("Type a number "))
+# gradeTwo = int(input("Type a second number "))
+# gradeThree = int(input("Type a third number "))
+# gradeFour = int(input("Type a fourth number "))
+
+# media = int((gradeOne + gradeTwo + gradeThree + gradeFour) / 4)
+
+# print("Sua média é: ", media)
+
+# --------------------------
+
+
+# DOIS ^^^^^^^^^^^^^^^^^^^^^^
+#Faça um programa que converta metros para milímetros.
+
+# meter = float(input("type the meter: "))
+
+# militers = float(meter * 1000)
+
+# print("you meter in militers is: ", militers)
+
+# --------------------------
+
+
+# TRES ^^^^^^^^^^^^^^^^^^^^^^
+# Faça um programa que peça o raio de um círculo, calcule e mostre sua área.
+
+# circle = float(input("Type de radius of a circle: "))
+
+# area = 3.14 * (circle * circle)
+
+# print("Your area is: ", area)
+
+# --------------------------
+
+# CAPITULO DOIS
+
+# EXERCICIOS
+
+# v1 = input()
+# v2 = input()
+
+# numberOne = v1
+# numberTwo = v2
+
+# v1 = numberTwo
+# v2 = numberOne
+
+# print('valor em v1:', v1)
+# print('valor em v2:', v2)
+
+# heigthOne = float(input())
+# heigthTwo = float(input())
+# heigthThree = float(input())
+# heigthFour = float(input())
+
+# if all([heigthOne, heigthTwo, heigthThree, heigthFour]) is not None:
+#     media = (heigthOne + heigthTwo + heigthThree + heigthFour) / 4
+# print(media)
+
+# fahrenheit = float(input())
+
+# celsius = (fahrenheit - 32) * 5/9
+# kelvin = celsius + 273.15
+
+# print('Convertendo ', celsius, ' graus Fahrenheit temos: celsius, ' graus Celsius e ', kelvin, ' Kelvin')
+
+polegadas = float(input())
+
+milimetros = polegadas * 24.5
+
+print(polegadas, "polegada(s) eh o mesmo que", milimetros, "mm")
